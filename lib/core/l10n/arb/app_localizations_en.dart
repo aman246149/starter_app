e4bdd85fd27fe4047706ff105d7e8b3240a1af53
eminsahinradity <seeminsahin@gmail.com>
2026-01-23 09:47:52 +0300

    feat(l10n): add localization implementation

---
+++ lib/core/l10n/arb/app_localizations_en.dart
@@ -0,0 +1,28 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appName => 'Starter App';
+
+  @override
+  String get unexpectedError => 'An unexpected error occurred';
+
+  @override
+  String get serverError => 'Something went wrong. Please try again.';
+
+  @override
+  String get networkError => 'Couldn\'t connect to the server. Please check your connection';
+
+  @override
+  String get cacheError => 'Local storage error';
+
+  @override
+  String get parseError => 'Invalid data format received. Please contact support.';
+}
